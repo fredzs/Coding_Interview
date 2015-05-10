5c346c4596cdb070197f2143262a58a8e95e7ea0
fredzs <fred_zs@163.com>
2015-05-10 17:57:02 +0800

    1.0 剑指Offer

---
+++ FirstNotRepeatingChar/FirstNotRepeatingChar.cpp
@@ -0,0 +1,77 @@
+// FirstNotRepeatingChar.cpp : Defines the entry point for the console application.
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 著作权所有者：何海涛
+
+#include "stdafx.h"
+#include <string>
+#include <unordered_map>
+using namespace std;
+char FirstNotRepeatingChar(char* pString)
+{
+    if(pString == NULL)
+        return '\0';
+
+    const int tableSize = 256;
+    unsigned int hashTable[tableSize];
+    for(unsigned int i = 0; i<tableSize; ++ i)
+        hashTable[i] = 0;
+
+    char* pHashKey = pString;
+    while(*(pHashKey) != '\0')
+        hashTable[*(pHashKey++)] ++;
+
+    pHashKey = pString;
+    while(*pHashKey != '\0')
+    {
+        if(hashTable[*pHashKey] == 1)
+            return *pHashKey;
+
+        pHashKey++;
+    }
+
+    return '\0';
+} 
+char FirstNotRepeatChar2(char* pString)
+{
+	if (pString == '\0')
+		return '\0';
+	string ss = pString;
+
+	unordered_map<char, int> mm;
+	for each (char c in ss)
+		mm[c] = 0;
+	for each (char c in ss)
+		mm[c]++;
+	for each (auto ii in mm)
+	{
+		if (ii.second == 1)
+			return ii.first;
+	}
+	return '\0';
+}
+// ====================测试代码====================
+void Test(char* pString, char expected)
+{
+	if (FirstNotRepeatChar2(pString) == expected)
+        printf("Test passed.\n");
+    else
+        printf("Test failed.\n");
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    // 常规输入测试，存在只出现一次的字符
+    Test("google", 'l');
+
+    // 常规输入测试，不存在只出现一次的字符
+    Test("aabccdbd", '\0');
+
+    // 常规输入测试，所有字符都只出现一次
+    Test("abcdefg", 'a');
+
+    // 鲁棒性测试，输入NULL
+    Test(NULL, '\0');
+
+    return 0;
+}
+
